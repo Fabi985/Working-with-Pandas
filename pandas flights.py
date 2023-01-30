a1a22083882acaf0559cf724e67da9cbd02160c4
fabi <115619516+Fabi985@users.noreply.github.com>
2023-01-30 15:31:18 +0000

    Months in order
    Changed it a bit so months are in order when GUI pop up instead of being in alphabetical order

--- pandas flights.py
+++ pandas flights.py
@@ -10,6 +10,9 @@ most_popular_by_month = extracted_data.groupby(extracted_data["Month"]).mean()
 print(extracted_data["Month"])
 print(most_popular_by_month)
 
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+most_popular_by_month = most_popular_by_month.reindex(new_order, axis=0)
 
 plt.plot(most_popular_by_month.drop(['Commission (%)'], axis=1))
 plt.legend(["Mean Price"])
